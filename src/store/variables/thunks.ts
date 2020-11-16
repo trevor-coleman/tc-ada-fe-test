8b02e2b184e93bd5b5372944f0b4864784b81b24
Trevor Coleman <trevor@trevorcoleman.design>
2020-11-16 11:47:26 -0500

    Remove console.log

--- src/store/variables/thunks.ts
+++ src/store/variables/thunks.ts
@@ -5,6 +5,5 @@ export const fetchVariables = createAsyncThunk('variables/fetch', async (_,
                                                                          ThunkApi) => {
   //TODO: Handle Failed Request with RejectWithValue()
   const result =  await api.variables.find()
-  console.log(result);
   return result;
 })
