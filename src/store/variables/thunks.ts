3a8edc38b2438676d0e1259f208530568fb6e6be
Trevor Coleman <trevor@trevorcoleman.design>
2020-11-17 15:10:09 -0500

    Handle rejected promise.

--- src/store/variables/thunks.ts
+++ src/store/variables/thunks.ts
@@ -2,8 +2,11 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import api from '../../api';
 
 export const fetchVariables = createAsyncThunk('variables/fetch', async (_,
-                                                                         ThunkApi) => {
-  //TODO: Handle Failed Request with RejectWithValue()
-  const result =  await api.variables.find()
-  return result;
+                                                                         thunkApi) => {
+  try {
+    return await api.variables.find();
+  } catch (e) {
+    thunkApi.rejectWithValue(e);
+  }
+  return [];
 })
